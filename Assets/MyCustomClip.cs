b721ebe507de7590fb8ca6ab89893939398824e4
Saeed Barari <s.barari@medrick.info>
2022-06-30 00:42:14 +0430

    readme samples

---
+++ Assets/MyCustomClip.cs
@@ -0,0 +1,13 @@
+﻿using AnimFlex.Clipper;
+using UnityEngine;
+
+public class MyCustomClip : Clip
+{
+    [SerializeField] private string yourName = "Someone";
+
+    protected override void OnStart()
+    {
+        Debug.Log($"Hi {yourName}");
+        End();
+    }
+}
