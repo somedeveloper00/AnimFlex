28c026c58c57c82d10ee52e0637e20b76d7c4b31
Saeed Barari <brarari.saeed.am@gmail.com>
2022-08-23 14:43:25 +0430

    settings & prefs refactoref

---
+++ Core/AnimFlexInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace AnimFlex.Core
+{
+    public class AnimFlexInitializer : MonoBehaviour
+    {
+        [SerializeField]
+        private AnimFlexSettings m_animFlexSettings;
+        
+        private static AnimFlexInitializer m_instance;
+
+        private void Awake()
+        {
+            if (m_instance != null)
+            {
+                Debug.LogError(
+                    $"There should be only one instance of AnimFlexInitializer in the game." +
+                    $"the new instance will be destroyed!");
+                Destroy(gameObject);
+            }
+
+            m_instance = this;
+        }
+    }
+}
