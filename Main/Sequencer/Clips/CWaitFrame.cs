038d15aeb79c2eab6ed6954d8be4729791fe8c7a
Saeed Barari <barari.saeed.am@outlook.com>
2023-05-18 20:36:45 +0330

    cwaitframw betterified

--- Main/Sequencer/Clips/CWaitFrame.cs
+++ Main/Sequencer/Clips/CWaitFrame.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using UnityEngine;
 
 namespace AnimFlex.Sequencer.Clips {
     
     [DisplayName("Wait For Frames")]
     [Category("Misc/Wait Frames")]
+    [Serializable]
     public class CWaitFrame : Clip {
 
         [Tooltip( "The number of frames to wait before playing the next clip" )]
@@ -12,7 +14,7 @@ namespace AnimFlex.Sequencer.Clips {
 
         int _f = 0;
 
-        protected override void OnStart() { _f = frames; }
+        protected override void OnStart() => _f = frames;
         public override void OnEnd() { }
         public override bool hasTick() => true;
         public override void Tick(float deltaTime) {
